9ef66c818191d8335d5e3db456ab628ded86a9f0
Luis Fernando Cezar dos Santos <nandocezar22@gmail.com>
2022-03-17 14:44:24 -0300

    insert animation and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import './App.css';
+
+import FirstAndSecond from './components/FirstAndSecond';
+
+import GlobalStyles from './styles/global'
+import { Main, Section, Sticky } from './styles'
 
 function App() {
   return (
-    <></>
+    <>
+      <GlobalStyles />
+      <Main style={{height: '1610vh'}}>
+        <Section style={{height: '72.7%'}}>
+          <FirstAndSecond />
+        </Section>
+
+        <Section style={{height: '9.7%'}}>
+          <Sticky className="third" />
+        </Section>
+
+        <Section style={{height: '10.1%'}}>
+          <Sticky className="fourth" />
+        </Section>
+      </Main>
+    </>
   );
 }
 
